28a2a1a95acd66bf00ea7f0f7f7e1df3990bc5b0
Giulio <143119811+nomeforme@users.noreply.github.com>
2025-08-07 23:51:12 +0100

    token name and supply as parameters

--- contracts/NodeToken.sol
+++ contracts/NodeToken.sol
@@ -4,9 +4,14 @@ pragma solidity ^0.8.24;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract NodeToken is ERC20 {
-    uint256 public constant TOTAL_SUPPLY = 1000 * 10**18; // 1000 tokens with 18 decimals
+    uint256 public immutable TOTAL_SUPPLY;
     
-    constructor() ERC20("NODE", "NODE") {
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint256 totalSupply
+    ) ERC20(name, symbol) {
+        TOTAL_SUPPLY = totalSupply * 10**18; // Convert to wei (18 decimals)
         _mint(msg.sender, TOTAL_SUPPLY);
     }
 }
