958baa912b9459c0fd86d2bcdb8b67f49470ce96
Giulio <143119811+nomeforme@users.noreply.github.com>
2025-09-01 23:01:29 +0100

    sepolia

--- scripts/TestDeploy.s.sol
+++ scripts/TestDeploy.s.sol
@@ -9,7 +9,8 @@ import "../contracts/NodeToken.sol";
 
 contract TestDeploy is Script {
     function run() external {
-        vm.startBroadcast(0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80);
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
         
         console.log("Deploying ERC6551Registry...");
         ERC6551Registry registry = new ERC6551Registry();
